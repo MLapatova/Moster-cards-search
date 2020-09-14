4b9939c278b33e4869f9160e5a42e65347f8dcff
MLapatova <marieta.lapatova@abv.bg>
2020-09-14 19:42:15 +0300

    Updates

---
+++ precache-manifest.8082b5a6b2401213c5f113f430c979eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0482d622c99f2cf4b72f581f7bf9def2",
+    "url": "/Moster-cards-search/index.html"
+  },
+  {
+    "revision": "8ed414a287a23d53779d",
+    "url": "/Moster-cards-search/static/css/main.57aef949.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Moster-cards-search/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Moster-cards-search/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed414a287a23d53779d",
+    "url": "/Moster-cards-search/static/js/main.542cb038.chunk.js"
+  },
+  {
+    "revision": "69fdf2b155f54cdad07f",
+    "url": "/Moster-cards-search/static/js/runtime-main.02e88ae1.js"
+  }
+]);
